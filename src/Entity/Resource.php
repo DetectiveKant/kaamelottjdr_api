c22307acb334780e057e30cb11e5d8176d869abb
alderi-tokori <vincent.bragard@gmail.com>
2023-02-18 17:25:01 +0100

    feat: ajout ressource Resource

---
+++ src/Entity/Resource.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\ResourceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ResourceRepository::class)]
+#[ApiResource]
+class Resource
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $rgbHexColor = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRgbHexColor(): ?string
+    {
+        return $this->rgbHexColor;
+    }
+
+    public function setRgbHexColor(string $rgbHexColor): self
+    {
+        $this->rgbHexColor = $rgbHexColor;
+
+        return $this;
+    }
+}
